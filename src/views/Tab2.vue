521d4dbcc406c74ddfd46f3935480a6cab0ded96
ivansmb11 <ismb9@hotmail.com>
2023-05-04 12:09:07 -0600

    fixed app

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Pronóstico</ion-title>
+        <ion-title>Forecast</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Pronóstico</ion-title>
+          <ion-title size="large">Forecast</ion-title>
         </ion-toolbar>
       </ion-header>
 
